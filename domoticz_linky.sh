393172fe59b15317ca6fe8871276740ba1c37614
epierre <epierre@e-nef.com>
2017-11-07 22:00:48 +0100

    added pi path

--- domoticz_linky.sh
+++ domoticz_linky.sh
@@ -9,4 +9,4 @@ cd $BASE_DIR
 python3 $BASE_DIR/linky_json.py -o "$BASE_DIR" >> $BASE_DIR/linky.log 2>&1
 
 node $BASE_DIR/domoticz_linky.js > req.sql
-cat $BASE_DIR/req.sql | sqlite3 /home/odroid/domoticz/domoticz.db
+cat $BASE_DIR/req.sql | sqlite3 /home/pi/domoticz/domoticz.db
